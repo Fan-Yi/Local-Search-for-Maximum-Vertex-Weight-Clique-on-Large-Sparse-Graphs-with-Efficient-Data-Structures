d09649885dbf01554240f991610860571bd1d350
Yi Fan <435980444@qq.com>
2016-09-21 09:25:57 +1000

    Add files via upload

---
+++ LMY-GRS.cpp
@@ -0,0 +1,39 @@
+/************************************************
+** This is a local search solver for Maximum Vertex Weight Clique.                                                       
+************************************************/
+
+
+/************************************************
+** Date:	2016.9.21  
+** LMY-GRS
+** Author: Yi Fan, yi.fan4@griffithuni.edu.au   
+**		   Institute for Integrated and Intelligent Systems,
+**		   Griffith University, 
+**		   Brisbane, Australia                                                                        
+************************************************/
+
+#include <iostream>
+#include "localSearch.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	char filename[1024];
+	sscanf(argv[1], "%s", filename);
+
+	int seed;
+	sscanf(argv[2], "%d", &seed);
+	srand(seed);
+
+	int time_limit;
+	sscanf(argv[3], "%d", &time_limit);
+
+	StochasticLocalSearch mySLS(filename, time_limit);
+
+	mySLS.clique_sls();
+
+	mySLS.show_results();
+
+	return 0;
+}
